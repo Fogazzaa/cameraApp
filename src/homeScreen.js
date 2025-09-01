503cd344e4bc2432fdb4a7ed495da4aa64efb35a
fogazza <vfogacacintra@gmail.com>
2025-09-01 09:22:37 -0300

    01-09-25 / feat: Implementando Monitoramento de Botão

--- src/homeScreen.js
+++ src/homeScreen.js
@@ -1,21 +1,41 @@
 import React from "react";
 import { View, Button } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import api from "./services/api";
 
 const HomeScreen = () => {
+  const [stateLed, setStateLed] = useState({ value: false });
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  const handleCam = () => {
+    navigation.navigate("Cam");
+  };
 
-    const handleCam = () => {
-        navigation.navigate("Cam")
+  async function ledToogle() {
+    try {
+      const response = await api.toggleLed({ value: `${!stateLed.value}` });
+      setStateLed({ value: !stateLed.value });
+      console.log("Resposta: ", response.data);
+    } catch (error) {
+      console.log("Erro: ", error.response.data);
     }
+  }
 
-    return(
-        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-            <Button title="Abrir Câmera" onPress={handleCam} color='black'/>
-        </View>
-    )
-
-}
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ marginBottom: 20 }}>
+        <Button title="Abrir Câmera" onPress={handleCam} color="black" />
+      </View>
+      <View>
+        <Button
+          title={stateLed.value ? "Desligar LED" : "Ligar LED"}
+          onPress={ledToogle}
+          color={stateLed.value ? "red" : "green"}
+        />
+      </View>
+    </View>
+  );
+};
 
 export default HomeScreen;
